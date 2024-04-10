0831c59ed0e28d979393fddde1ea60a52c7d7b0b
Nathan Selikoff <nselikoff@gmail.com>
2024-04-10 08:13:09 -0400

    Group by type and exhibition

--- export.js
+++ export.js
@@ -3,7 +3,9 @@ const { exportArtCatalogXml } = require("./art");
 const { exportFilmCatalogXml } = require("./film");
 const { exportFashionCatalogXml } = require("./fashion");
 
-fs.mkdirSync("export");
+fs.mkdirSync("export/art", { recursive: true });
+fs.mkdirSync("export/film", { recursive: true });
+fs.mkdirSync("export/fashion", { recursive: true });
 
 const artSubmissionsMap = exportArtCatalogXml(
   fs.readFileSync("./catalog-export.xml", {
@@ -11,11 +13,9 @@ const artSubmissionsMap = exportArtCatalogXml(
     flag: "r",
   }),
 );
-const artFilename = "export/catalog-export-art.json";
-fs.writeFileSync(
-  artFilename,
-  JSON.stringify(Array.from(artSubmissionsMap.values())),
-);
+const artFilename = "export/art/all-art-submissions.json";
+const artSubmissions = Array.from(artSubmissionsMap.values());
+fs.writeFileSync(artFilename, JSON.stringify(artSubmissions));
 console.log(`${artFilename} created`);
 
 const filmSubmissionsMap = exportFilmCatalogXml(
@@ -24,11 +24,9 @@ const filmSubmissionsMap = exportFilmCatalogXml(
     flag: "r",
   }),
 );
-const filmFilename = "export/catalog-export-film.json";
-fs.writeFileSync(
-  filmFilename,
-  JSON.stringify(Array.from(filmSubmissionsMap.values())),
-);
+const filmFilename = "export/film/all-film-submissions.json";
+const filmSubmissions = Array.from(filmSubmissionsMap.values());
+fs.writeFileSync(filmFilename, JSON.stringify(filmSubmissions));
 console.log(`${filmFilename} created`);
 
 const fashionSubmissionsMap = exportFashionCatalogXml(
@@ -37,7 +35,7 @@ const fashionSubmissionsMap = exportFashionCatalogXml(
     flag: "r",
   }),
 );
-const fashionFilename = "export/catalog-export-fashion.json";
+const fashionFilename = "export/fashion/all-fashion-submissions.json";
 const fashionSubmissions = Array.from(fashionSubmissionsMap.values());
 fashionSubmissions.forEach((submission) => {
   submission.fashion_items = Array.from(submission.fashionItemsMap.values());
@@ -47,3 +45,48 @@ fashionSubmissions.forEach((submission) => {
 });
 fs.writeFileSync(fashionFilename, JSON.stringify(fashionSubmissions));
 console.log(`${fashionFilename} created`);
+
+const artSubmissionsByExhibition = new Map();
+artSubmissions.forEach((submission) => {
+  const existingSubmissions =
+    artSubmissionsByExhibition.get(submission.exhibition) || [];
+  artSubmissionsByExhibition.set(submission.exhibition, [
+    ...existingSubmissions,
+    submission,
+  ]);
+});
+artSubmissionsByExhibition.forEach((submissions, exhibition) => {
+  const filename = `export/art/${exhibition}.json`;
+  fs.writeFileSync(filename, JSON.stringify(submissions, null, 2));
+  console.log(`${filename} created`);
+});
+
+const filmSubmissionsByExhibition = new Map();
+filmSubmissions.forEach((submission) => {
+  const existingSubmissions =
+    filmSubmissionsByExhibition.get(submission.exhibition) || [];
+  filmSubmissionsByExhibition.set(submission.exhibition, [
+    ...existingSubmissions,
+    submission,
+  ]);
+});
+filmSubmissionsByExhibition.forEach((submissions, exhibition) => {
+  const filename = `export/film/${exhibition}.json`;
+  fs.writeFileSync(filename, JSON.stringify(submissions, null, 2));
+  console.log(`${filename} created`);
+});
+
+const fashionSubmissionsByExhibition = new Map();
+fashionSubmissions.forEach((submission) => {
+  const existingSubmissions =
+    fashionSubmissionsByExhibition.get(submission.exhibition) || [];
+  fashionSubmissionsByExhibition.set(submission.exhibition, [
+    ...existingSubmissions,
+    submission,
+  ]);
+});
+fashionSubmissionsByExhibition.forEach((submissions, exhibition) => {
+  const filename = `export/fashion/${exhibition}.json`;
+  fs.writeFileSync(filename, JSON.stringify(submissions, null, 2));
+  console.log(`${filename} created`);
+});
